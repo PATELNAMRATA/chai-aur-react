24d7e9e57266cc36d27b98b8433433e0322c5d67
npatel <namrata.p@yalabs.in>
2024-08-02 15:09:10 +0530

    Counter increase decrese project with use state

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom app</h1>
+        </div>
+    )
+}
+
+const reactElement =React.createElement(
+    'a',
+    {href:'https://google.com' , target:'_blank'},
+    'click me to visit goolge'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <MyApp />
 )
---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [counter , setCounter] = useState(15)
+  //to set initial counter value is 5
+  // let counter = 5;
+
+ const addValue = () =>{
+  // console.log("value added", Math.random());
+  if (counter < 20) {
+    setCounter(counter + 1)
+  }else{
+    alert("20 is max")
+  }
+  // console.log("Clicked", setCounter(counter));
+
+ }
+
+ const removeValue = () => {
+  if (counter > 0) {
+    setCounter(counter - 1)
+  }else{
+    alert("0 is min")
+
+  }
+  // console.log("remove",counter);
+ }
+
+  return (
+    <>
+    <h1>chai aur react</h1>
+    {/* use counter variable here to display value of counter variable */}
+    <h2>Counter value : {counter}</h2>
+    <button onClick={addValue}>Add value : {counter}</button>
+    <br />
+    <button id="IdrmvBtn" onClick={removeValue}>Remove value : {counter}</button>
+    
+
+    </>
+  )
+}
+
+export default App
