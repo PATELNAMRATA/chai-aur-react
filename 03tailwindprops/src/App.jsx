433dbe3984513754ca76f74fe2be09dbce7c1572
namrata <patelnamu13@gmail.com>
2024-08-24 11:47:14 +0530

    Complete Tailwind css and Props concept 24-8-24

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './Components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   <h1 className='bg-green-400'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="Chai Aur Code" btnText="Click me"  />
+      <Card username="Dolba" btnText="Visit me"/>
+
     </>
   )
 }
